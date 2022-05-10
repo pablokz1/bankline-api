5bc9c091114077e5d5d0bef0a21b181e5901644c
Pablo Carvalho <pablo.carvalho16@hotmail.com>
2022-05-10 10:43:40 -0300

    Integrando banco de dados com spring boot

--- src/main/java/com/dio/santander/bankline/api/Controller/CorrentistaController.java
+++ src/main/java/com/dio/santander/bankline/api/Controller/CorrentistaController.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dio.santander.bankline.api.dto.NovoCorrentista;
 import com.dio.santander.bankline.api.model.Correntista;
 import com.dio.santander.bankline.api.repository.CorrentistaRepository;
+import com.dio.santander.bankline.api.service.CorrentistaService;
 
 
 @RestController
@@ -18,9 +22,17 @@ public class CorrentistaController {
 	@Autowired
 	private CorrentistaRepository repository;
 	
+	@Autowired
+	private CorrentistaService service;
+	
 	@GetMapping
 	public List<Correntista> findAll(){
 		return repository.findAll();
+	}
+	
+	@PostMapping
+	public void save(@RequestBody NovoCorrentista correntista) {
+		service.save(correntista);
 	}
 
 }
